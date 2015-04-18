72176714b5e242e7f72b099aa26e22210e1243cf
Eirik S. Morland <eirik@morland.no>
2015-04-18 17:20:14 +0200

    Fix some tests that really should pass.

--- tests/testFrontpage.js
+++ tests/testFrontpage.js
@@ -2,7 +2,12 @@
 var system = require('system'); 
 casper.test.begin('The front page is reachable', function(test) {
   casper.start(system.env.TEST_URL, function() {
-    test.assertExists('h2.node__title');
+    // We should have that stupid lightning cloud there.
+    test.assertExists('#cloudLightningFill', 'The cloud with lightning is present on the front page');
+    // There should be more than one article on the front page.
+    test.assert(1 < casper.evaluate(function() {
+      return document.querySelectorAll('.views-field-title a').length;
+    }), 'There is more than 1 article on the front page');
   }).run(function() {
     test.done();
   });
