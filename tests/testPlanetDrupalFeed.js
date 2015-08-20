f62ed3ed23baf5518aea08e4784075cf1c4e7e7c
Eirik S. Morland <eirik@morland.no>
2015-08-20 16:27:06 +0200

    Add test for taxonomy terms (Fix #10).

--- tests/testPlanetDrupalFeed.js
+++ tests/testPlanetDrupalFeed.js
@@ -3,7 +3,6 @@ var system = require('system');
 casper.test.begin('The planet Drupal feed is reachable', function(test) {
   casper.start(system.env.TEST_URL + '/planet', function() {
     test.assertHttpStatus(200, 'The feed is reachable and does not give an error');
-    this.debugPage();
   }).run(function() {
     test.done();
   });
