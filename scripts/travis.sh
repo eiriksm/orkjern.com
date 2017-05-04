c3c6625b97b3d88c81dd04d467ad8139e5bb8ca9
eiriksm <eirik@morland.no>
2017-05-04 18:09:44 +0200

    Update travis.sh

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -3,7 +3,7 @@ composer install
 cd web
 php -d sendmail_path=`which true` ../vendor/bin/drush.php si minimal --db-url="mysql://$DB_USERNAME@127.0.0.1/$DATABASE" -y
 ../vendor/bin/drush cset system.site uuid 5ffb47de-ba4e-4ba3-8a97-3c7bcfbdfa84 -y
-../vendor/bin/drush cim staging -y
+../vendor/bin/drush cim -y
 ../vendor/bin/drush download-nodes
 ../vendor/bin/drush import-nodes
 ../vendor/bin/drush runserver 127.0.0.1:8080 &
