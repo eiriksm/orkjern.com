d18c4c5bcf1f6f6c7c555d5a94e0ec041e74ca80
Eirik S. Morland <eirik@morland.no>
2018-06-07 19:33:48 +0200

    Better grep exclusion

--- scripts/travis-check.sh
+++ scripts/travis-check.sh
@@ -1,4 +1,4 @@
 set -e
 ./vendor/bin/drush cex -y
 git status
-[[ -z $(git status -s | grep -v "modified:   web/sites/default/settings.php") ]] || exit 1
+[[ -z $(git status -s | grep -v "web/sites/default/settings.php") ]] || exit 1
