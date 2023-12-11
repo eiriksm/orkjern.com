ed46c962e5a8a34cc8f856c209e51af6a0f79b96
Eirik Stanghelle Morland <eirik@morland.no>
2023-12-11 19:00:28 +0100

    Switch to recommended (#213)

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -754,3 +754,4 @@ if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
 $config_directories['sync'] = '../config';
+$settings['config_sync_directory'] = '../config';
