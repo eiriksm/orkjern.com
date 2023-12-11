064623902e3f41179bde3ca56c52d3e629e07089
Eirik S. Morland <eirik@morland.no>
2023-12-11 15:13:33 +0100

    Ah we dont use containers

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -6,8 +6,8 @@ npm install chromedriver@2.35
 ./node_modules/.bin/chromedriver --port=8643 --url-base=wd/hub &
 composer install
 cd web
-mysql -u root -proot -h db -e "CREATE DATABASE db;"
-php -d sendmail_path=`which true` ../vendor/bin/drush si minimal --db-url="mysql://root:root@db:3306/db" -y
+mysql -u root -proot -h 127.0.0.1 -e "CREATE DATABASE db;"
+php -d sendmail_path=`which true` ../vendor/bin/drush si minimal --db-url="mysql://root:root@127.0.0.1:3306/db" -y
 ../vendor/bin/drush cset system.site uuid 5ffb47de-ba4e-4ba3-8a97-3c7bcfbdfa84 -y
 ../vendor/bin/drush cim -y
 ../vendor/bin/drush download-nodes
